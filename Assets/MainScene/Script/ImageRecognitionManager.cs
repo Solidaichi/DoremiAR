fc9b267953f1f094ae4779529b7905fb82aacc2d
dk78692 <solideye.2014@gmail.com>
2020-03-17 01:14:16 +0900

    マーカーARをまとめるプログラム(ImageRecognitionManager)を追加 pianoAR.prefab追加 ローカルキャッシュを設定

---
+++ Assets/MainScene/Script/ImageRecognitionManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognitionManager : MonoBehaviour
+{
+
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();    
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+
+    }
+
+}
