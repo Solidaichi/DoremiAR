fac5f5293838363ca28bd00bcf58d5f6dd4c5f0b
dk78692 <solideye.2014@gmail.com>
2020-03-31 19:56:46 +0900

    スクリプト追加

---
+++ Assets/MainScene/Script/ARRaycastManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARRaycastManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("AR空間に表示させたいプレハブを登録")] GameObject _ARobj;
+
+    private GameObject _SpawnedObj;
+    private ARRaycastManager raycastManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.touchCount > 0)
+        {
+            Vector2 _TouchPosition = Input.GetTouch(0).position;
+            
+            if (raycastManager.Raycast(_TouchPosition, hits, TrackableType.Planes))
+            {
+
+            }
+        } 
+    }
+}
