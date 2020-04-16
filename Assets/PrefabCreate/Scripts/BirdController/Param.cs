42159325231a44e162276f3e0f06d63fdbbfa104
dk78692 <solideye.2014@gmail.com>
2020-04-16 20:46:40 +0900

    BirdController再構築

---
+++ Assets/PrefabCreate/Scripts/BirdController/Param.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boid/Param")]
+public class Param : ScriptableObject
+{
+    public float initSpeed;
+    public float minSpeed;
+    public float maxSpeed;
+    public float neighborDistance;
+    public float neighborFov;
+    public float separationWeight;
+    public float wallScale;
+    public float wallDistance;
+    public float wallWeight;
+    public float alignmentWeight;
+    public float cohesionWeiight;
+}
