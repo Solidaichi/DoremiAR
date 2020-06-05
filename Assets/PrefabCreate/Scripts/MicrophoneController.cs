061d39b8b55fc7386f1106ac59d35d16485440a7
dk78692 <solideye.2014@gmail.com>
2020-06-05 23:18:13 +0900

    録音スクリプト導入（未実験）

---
+++ Assets/PrefabCreate/Scripts/MicrophoneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))] //AudioSourceは必須.
+[DisallowMultipleComponent]     // 複数アタッチできないようにするため
+public class MicrophoneController : MonoBehaviour
+{
+    void Start()
+    {
+        AudioSource aud = GetComponent<AudioSource>();
+        // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
+        aud.clip = Microphone.Start(null, true, 10, 44100);
+        aud.Play();
+    }
+
+    void Update()
+    {
+        float[] spectrum = new float[256];
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        var maxIndex = 0;
+        var maxValue = 0.0f;
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            var val = spectrum[i];
+            if (val > maxValue)
+            {
+                maxValue = val;
+                maxIndex = i;
+            }
+        }
+        // maxValue が最も大きい周波数成分の値で、
+        // maxIndex がそのインデックス。欲しいのはこっち。
+
+        //音声出力のサンプリングレートをF,`spectrum`の長さをQとすると * *`spectrum[N]`には`N* F/ 2 / Q`Hzの周波数成分が含まれています。
+        var freq = maxIndex * AudioSettings.outputSampleRate / 2 / spectrum.Length;
+    }
+}
---
+++ Assets/PrefabCreate/Scripts/NoteNameDetector.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class NoteNameDetector
+{
+    private string[] noteNames = { "ド", "ド♯", "レ", "レ♯", "ミ", "ファ", "ファ♯", "ソ", "ソ♯", "ラ", "ラ♯", "シ" };
+
+    public string GetNoteName(float freq)
+    {
+        // 周波数からMIDIノートナンバーを計算
+        var noteNumber = calculateNoteNumberFromFrequency(freq);
+        // 0:C - 11:B に収める
+        var note = noteNumber % 12;
+        // 0:C～11:Bに該当する音名を返す
+        return noteNames[note];
+    }
+
+    // See https://en.wikipedia.org/wiki/MIDI_tuning_standard
+    private int calculateNoteNumberFromFrequency(float freq)
+    {
+        return Mathf.FloorToInt(69 + 12 * Mathf.Log(freq / 440, 2));
+    }
+}
