a2c9d57bde8356bb8fe182916ab63eae47a7512d
dk78692 <solideye.2014@gmail.com>
2020-06-10 09:15:17 +0900

    書き直し（マイクが取れない問題）

--- Assets/PrefabCreate/Scripts/MicrophoneController.cs
+++ Assets/PrefabCreate/Scripts/MicrophoneController.cs
@@ -7,42 +7,52 @@ using UnityEngine.UI;
 [DisallowMultipleComponent]     // 複数アタッチできないようにするため
 public class MicrophoneController : MonoBehaviour
 {
-    NoteNameDetector noteName;
-    Text noteText;
+    // 波形を描画する
+    public LineRenderer line;
+
+    // マイクからの音を拾う
+    public AudioSource mic;
+    private string mic_name = "UAB-80";
+
+    // 波形描画のための変数
+    private float[] wave;
+    private int wave_num;
+    private int wave_count;
+
 
     void Start()
     {
-        noteName = new NoteNameDetector();
-        noteText = GameObject.FindWithTag("noteText").GetComponent<Text>();
-        AudioSource aud = GetComponent<AudioSource>();
-        // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
-        aud.clip = Microphone.Start(null, true, 10, 44100);
-        aud.Play();
+        // 波形描画のための変数の初期化
+        wave_num = 300;
+        wave = new float[wave_num];
+        wave_count = 0;
+
+        // micにマイクを割り当てる
+        mic.clip = Microphone.Start(mic_name, true, 999, 44100);
+        if (mic.clip == null)
+        {
+            Debug.LogError("Microphone.Start");
+        }
+        mic.loop = true;
+        mic.mute = true;
+
+        // 録音の準備が出来るまで待つ
+        while (!(Microphone.GetPosition(mic_name) > 0)) { }
+        mic.Play();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float[] spectrum = new float[256];
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        // 諸々の解析
+        float hertz = NoteNameDetector.AnalyzeSound(mic, 1024, 0.04f);
+        float scale = NoteNameDetector.ConvertHertzToScale(hertz);
+        string s = NoteNameDetector.ConvertScaleToString(scale);
+        Debug.Log(hertz + "Hz, Scale:" + scale + ", " + s);
 
-        var maxIndex = 0;
-        var maxValue = 0.0f;
-        for (int i = 0; i < spectrum.Length; i++)
-        {
-            var val = spectrum[i];
-            if (val > maxValue)
-            {
-                maxValue = val;
-                maxIndex = i;
-            }
-        }
-        // maxValue が最も大きい周波数成分の値で、
-        // maxIndex がそのインデックス。欲しいのはこっち。
-
-        //音声出力のサンプリングレートをF,`spectrum`の長さをQとすると * *`spectrum[N]`には`N* F/ 2 / Q`Hzの周波数成分が含まれています。
-        var freq = maxIndex * AudioSettings.outputSampleRate / 2 / spectrum.Length;
-
-        string noteNamesText = noteName.GetNoteName(freq);
-        noteText.text = noteNamesText;
+        // 波形描画
+        wave[wave_count] = scale;
+        NoteNameDetector.ScaleWave(wave, wave_count, line);
+        wave_count++;
+        if (wave_count >= wave_num) wave_count = 0;
     }
 }
--- Assets/PrefabCreate/Scripts/NoteNameDetector.cs
+++ Assets/PrefabCreate/Scripts/NoteNameDetector.cs
@@ -1,42 +1,111 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class NoteNameDetector
+
+
+public static class NoteNameDetector
 {
-    private string[] noteNames = { "ド", "ド♯", "レ", "レ♯", "ミ", "ファ", "ファ♯", "ソ", "ソ♯", "ラ", "ラ♯", "シ", ""};
-
-    public string GetNoteName(float freq)
+    // オーディオの周波数を調べる
+    // ac: 解析したいオーディオソース
+    // qSamples: 解析結果のサイズ
+    // threshold: ピッチの閾値
+    public static float AnalyzeSound(AudioSource ac, int qSamples, float threshold)
     {
-        // 周波数からMIDIノートナンバーを計算
-        var noteNumber = calculateNoteNumberFromFrequency(freq);
-        if (noteNumber == 12)
+        float[] spectrum = new float[qSamples];
+        ac.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+        float maxV = 0;
+        int maxN = 0;
+        //最大値（ピッチ）を見つける。ただし、閾値は超えている必要がある
+        for (int i = 0; i < qSamples; i++)
         {
-            return noteNames[12];
-        }else
-        {
-            // 0:C - 11:B に収める
-            var note = noteNumber % 12;
-            // 0:C～11:Bに該当する音名を返す
-            //Debug.Log("Note" + note);
-            return noteNames[note];
+            if (spectrum[i] > maxV && spectrum[i] > threshold)
+            {
+                maxV = spectrum[i];
+                maxN = i;
+            }
         }
-        
+
+        float freqN = maxN;
+        if (maxN > 0 && maxN < qSamples - 1)
+        {
+            //隣のスペクトルも考慮する
+            float dL = spectrum[maxN - 1] / spectrum[maxN];
+            float dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+
+        float pitchValue = freqN * (AudioSettings.outputSampleRate / 2) / qSamples;
+        return pitchValue;
     }
 
-    // See https://en.wikipedia.org/wiki/MIDI_tuning_standard
-    private int calculateNoteNumberFromFrequency(float freq)
+    // ヘルツから音階への変換
+    public static float ConvertHertzToScale(float hertz)
     {
-        int freqNote = Mathf.FloorToInt(69 + 12 * Mathf.Log(freq / 440, 2));
-        
-        if (freq <= 0)
+        float value = 0.0f;
+        if (hertz == 0.0f) return value;
+        else
         {
-            Debug.Log("Note" + freqNote);
-            return 12;
-
-        }else
-        {
-            return freqNote;
+            value = 12.0f * Mathf.Log(hertz / 110.0f) / Mathf.Log(2.0f);
+            // while (value <= 12.0f) value += 12.0f;
+            // while (value > 36.0f) value -= 12.0f;
+            return value;
         }
+    }
 
-        
+    // 数値音階から文字音階への変換
+    public static string ConvertScaleToString(float scale)
+    {
+        // 12音階の何倍の精度で音階を見るか
+        int precision = 2;
+
+        // 今の場合だと、mod24が0ならA、1ならAとA#の間、2ならA#…
+        int s = (int)scale;
+        if (scale - s >= 0.5) s += 1; // 四捨五入
+        s *= precision;
+
+        int smod = s % (12 * precision); // 音階
+        int soct = s / (12 * precision); // オクターブ
+
+        string value; // 返す値
+
+        if (smod == 0) value = "A";
+        else if (smod == 1) value = "A+";
+        else if (smod == 2) value = "A#";
+        else if (smod == 3) value = "A#+";
+        else if (smod == 4) value = "B";
+        else if (smod == 5) value = "B+";
+        else if (smod == 6) value = "C";
+        else if (smod == 7) value = "C+";
+        else if (smod == 8) value = "C#";
+        else if (smod == 9) value = "C#+";
+        else if (smod == 10) value = "D";
+        else if (smod == 11) value = "D+";
+        else if (smod == 12) value = "D#";
+        else if (smod == 13) value = "D#+";
+        else if (smod == 14) value = "E";
+        else if (smod == 15) value = "E+";
+        else if (smod == 16) value = "F";
+        else if (smod == 17) value = "F+";
+        else if (smod == 18) value = "F#";
+        else if (smod == 19) value = "F#+";
+        else if (smod == 20) value = "G";
+        else if (smod == 21) value = "G+";
+        else if (smod == 22) value = "G#";
+        else value = "G#+";
+        value += soct + 1;
+
+        return value;
+    }
+
+    // 数値音階から生波形を出す
+    public static void ScaleWave(float[] scale, int size, LineRenderer line)
+    {
+        line.SetVertexCount(size);
+        float x = -150.0f;
+        for (int i = 0; i < size; i++)
+        {
+            line.SetPosition(i, new Vector3(x, -80.0f + scale[i] * 2.5f, 0));
+            x += 1.0f;
+        }
     }
 }
