72262eed7fe66080318497290014da28c6386302
dk78692 <solideye.2014@gmail.com>
2020-06-11 07:39:42 +0900

    なんかできちゃった

--- Assets/PrefabCreate/Scripts/MicrophoneController.cs
+++ Assets/PrefabCreate/Scripts/MicrophoneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(AudioSource))] //AudioSourceは必須.
+
 [DisallowMultipleComponent]     // 複数アタッチできないようにするため
 public class MicrophoneController : MonoBehaviour
 {
@@ -11,8 +11,8 @@ public class MicrophoneController : MonoBehaviour
     public LineRenderer line;
 
     // マイクからの音を拾う
-    public AudioSource mic;
-    private string mic_name = "UAB-80";
+    private new AudioSource audio;
+    //private string mic_name = "UAB-80";
 
     // 波形描画のための変数
     private float[] wave;
@@ -27,24 +27,20 @@ public class MicrophoneController : MonoBehaviour
         wave = new float[wave_num];
         wave_count = 0;
 
-        // micにマイクを割り当てる
-        mic.clip = Microphone.Start(mic_name, true, 999, 44100);
-        if (mic.clip == null)
-        {
-            Debug.LogError("Microphone.Start");
-        }
-        mic.loop = true;
-        mic.mute = true;
+        audio = GetComponent<AudioSource>();
 
-        // 録音の準備が出来るまで待つ
-        while (!(Microphone.GetPosition(mic_name) > 0)) { }
-        mic.Play();
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 10, 44100);  // マイクからのAudio-InをAudioSourceに流す
+        audio.loop = true;                                      // ループ再生にしておく
+        //audio.mute = true;                                      // マイクからの入力音なので音を流す必要がない
+        while (!(Microphone.GetPosition("") > 0)) { }             // マイクが取れるまで待つ。空文字でデフォルトのマイクを探してくれる
+        audio.Play();
     }
 
     void Update()
     {
         // 諸々の解析
-        float hertz = NoteNameDetector.AnalyzeSound(mic, 1024, 0.04f);
+        float hertz = NoteNameDetector.AnalyzeSound(audio, 1024, 0.04f);
         float scale = NoteNameDetector.ConvertHertzToScale(hertz);
         string s = NoteNameDetector.ConvertScaleToString(scale);
         Debug.Log(hertz + "Hz, Scale:" + scale + ", " + s);
