5ac4d6262dc36c61ce57c4fed04d9135a9874dc2
dk78692 <solideye.2014@gmail.com>
2020-06-17 23:40:21 +0900

    音によって樹木を出現させることに成功 → 明後日、メインシーンに導入・テスト

--- Assets/PrefabCreate/Scripts/MicrophoneController.cs
+++ Assets/PrefabCreate/Scripts/MicrophoneController.cs
@@ -11,8 +11,6 @@ public class MicrophoneController : MonoBehaviour
     public LineRenderer line;
     public Text text;
 
-    GameObject natureObj, natureObj1;
-
     // マイクからの音を拾う
     private new AudioSource audio;
     //private string mic_name = "UAB-80";
@@ -25,6 +23,7 @@ public class MicrophoneController : MonoBehaviour
 
     void Start()
     {
+
         // 波形描画のための変数の初期化
         wave_num = 300;
         wave = new float[wave_num];
@@ -46,17 +45,37 @@ public class MicrophoneController : MonoBehaviour
         float hertz = NoteNameDetector.AnalyzeSound(audio, 1024, 0.04f);
         float scale = NoteNameDetector.ConvertHertzToScale(hertz);
         string s = NoteNameDetector.ConvertScaleToString(scale);
-        Debug.Log(hertz + "Hz, Scale:" + scale + ", " + s);
+        //Debug.Log(hertz + "Hz, Scale:" + scale + ", " + s);
+        //Debug.Log("A+");
         text.text = s;
 
         if (s == "A2")
         {
-            natureObj = GameObject.FindWithTag("A2");
-            natureObj.SetActive(true);
-        }else if (s == "D2")
+            var natureObj = GameObject.FindWithTag("A2").transform;
+            Debug.Log(natureObj.name);
+            foreach (Transform child in natureObj.transform)
+            {
+                Debug.Log(child.name);
+                child.gameObject.SetActive(true);
+            }
+        }
+        else if (s == "D2")
         {
-            natureObj1 = GameObject.FindWithTag("D2");
-            natureObj1.SetActive(true);
+            var natureObj2 = GameObject.FindWithTag("D2").transform;
+            foreach (Transform child in natureObj2.transform)
+            {
+                Debug.Log(child.name);
+                child.gameObject.SetActive(true);
+            }
+        }
+        else if (s == "C2")
+        {
+            var natureObj3 = GameObject.FindWithTag("C2").transform;
+            foreach (Transform child in natureObj3.transform)
+            {
+                Debug.Log(child.name);
+                child.gameObject.SetActive(true);
+            }
         }
 
 
